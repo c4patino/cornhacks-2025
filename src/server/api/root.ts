acf801ca5dea3c87b259367f9e03dbceb6022c95
C4 Patino <c4patino@gmail.com>
2025-03-01 20:04:21 -0600

    feat: added integrated server-side queries for chat

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,11 +1,23 @@
-import { createCallerFactory, createTRPCRouter } from "@/server/api/trpc";
+import { observable } from "@trpc/server/observable";
+import { on } from "events";
+
+import {
+  createCallerFactory,
+  createTRPCRouter,
+  publicProcedure,
+} from "@/server/api/trpc";
+
+import { chatRouter } from "@/server/api/routers/chat";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  healthcheck: publicProcedure.query(() => "ok"),
+  chat: chatRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
---
+++ src/server/api/routers/chat.ts
@@ -0,0 +1,20 @@
+import { on } from "events";
+
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { ee } from "@/trpc/shared";
+import { Message, MessageSchema } from "@/lib/types";
+
+export const chatRouter = createTRPCRouter({
+  send: publicProcedure.input(MessageSchema).mutation(({ input }) => {
+    ee.emit("send", input);
+  }),
+
+  get: publicProcedure.subscription(async function* (opts) {
+    for await (const [data] of on(ee, "send", {
+      signal: opts.signal,
+    })) {
+      const message = data as Message;
+      yield message;
+    }
+  }),
+});
